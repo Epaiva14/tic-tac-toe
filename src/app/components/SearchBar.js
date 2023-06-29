4fe79dcb2fe2efd0e7d91d20569041f9e15506bb
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-06-28 23:05:27 -0400

    feat: completed tic tac toe game

---
+++ src/app/components/SearchBar.js
@@ -0,0 +1,13 @@
+
+export default function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+            <label>
+                <input type="checkbox" />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
-import './globals.css'
+// import './globals.css'
+import './style.css'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
